2932adb4667c0bfe289cd7262a6a19310c163f0e
Kartikey Srivastava <84186493+Kartik1328@users.noreply.github.com>
2025-03-03 12:24:16 +0530

    Add files via upload

---
+++ constants/AppConstants.java
@@ -0,0 +1,16 @@
+package com.pms.constants;
+public class AppConstants {
+
+    public static final String ID_GEN_PADDING="CMSCMSCMSCMSCMSCMSCMS";
+    public static final int ID_GEN_FIRST_CHAR_LEN= 3;
+
+}
+
+//In our PMS module till now we have not used the concept of constants and for the IDs we have taken the normal auto
+//generated ones.
+//if required then we can implement like this.
+
+//use of constants:::
+//to generate auto primary key with the help of the string, and it takes the first three characters.
+//here in this project we are generating the primary key with the help of the string and here like other projects
+//primary key is not auto generated .
